525ff0e2ec58eb548fba52a4df617a5fab6a1add
amos03 <amossi@gmail.com>
2021-06-21 21:32:08 -0400

    Exercise 1 double(my_number) called multiple times with different numbers passed each time. Does not seem to accept all rational numbers such as 2/3.

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,16 @@
 # Define a method called double that accepts an argument called my_number and returns that number multiplied by 2.
 
 # Try calling it multiple times and pass in different numbers each time.
+
+def double(my_number)
+    p my_number * 2
+end
+
+fraction = 2/3
+
+double(24)
+double(11.5)
+double(-2)
+double (0)
+double (fraction)
+double(0.5)
