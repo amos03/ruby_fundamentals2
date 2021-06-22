f2a9349d9ccf5ec5d056c5431950a4ba1050718c
amos03 <amossi@gmail.com>
2021-06-21 21:21:46 -0400

    Exercise 1 file

---
+++ exercise1.rb
@@ -0,0 +1,3 @@
+# Define a method called double that accepts an argument called my_number and returns that number multiplied by 2.
+
+# Try calling it multiple times and pass in different numbers each time.
